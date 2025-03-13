c6fd9c0d86a316db2868cdf3718b1912f0105e22
TOOBA TARIQ <tooba.tariq@city.ac.uk>
2025-03-13 13:57:01 +0000

    Link and setup Database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+from configparser import ConfigParser
 import os
 
 load_dotenv()
@@ -95,13 +96,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Load config.ini file
+config = ConfigParser()
+config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'config.ini')
+config.read(config_path)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config['database']['NAME'],
+        'USER': config['database']['USER'],
+        'PASSWORD': config['database']['PASSWORD'],
+        'HOST': config['database']['HOST'],
+        'PORT': config['database']['PORT'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
